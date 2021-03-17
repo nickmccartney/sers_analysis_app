4902045ceea25cf3d5f7a9a6fe41a7ab7ed99692
nickmccartney <nickmcc3@gmail.com>
2021-03-17 18:57:54 -0400

    (WIP) more work on formalizing callback logic

--- index.py
+++ index.py
@@ -1,14 +1,12 @@
 import dash_html_components as html
 import dash_core_components as dcc
+import plotly.express as px
 
 from dash.dependencies import Input, Output
 from app import app
 from app import server
 
 import database_interface as dbi
-
-### NOTE:TEMPORARY, have external function handle
-import pandas as pd
 
 app.layout = html.Div([
         dcc.Tabs(
@@ -24,10 +22,12 @@ app.layout = html.Div([
     ], style={"background": "#ffffff"}
 )
 
+
 @app.callback(Output('tab-content', 'children'),
               Input('tab-index', 'value'))
 def render_content(tab):
     if tab == 'tab-1':
+
         dataset_options = [{'label': name, 'value': name} for name in dbi.list_datasets()]
         dataset_options.append({'label': 'Create New (enter name below...)', 'value': 'NEW'})
 
@@ -35,23 +35,14 @@ def render_content(tab):
 
             html.Label("Dataset"),
             dcc.Dropdown(
-                id='dataset-selection',
+                id='select-dataset',
                 options=dataset_options,
                 placeholder='Select a Dataset...'
             ),
 
-            html.Div(id='dataset-entry'),
+            html.Div(id='label-entry'),
 
-            dcc.Graph(
-                id='graph-1',
-                figure={
-                    'data': [
-                        {'x': [1, 2, 3, 4, 5, 6, 7], 'y': [10, 20, 30, 40, 50, 60, 70], 'type': 'line', 'name': 'value1'},
-                        {'x': [1, 2, 3, 4, 5, 6, 7], 'y': [12, 22, 36, 44, 49, 58, 73], 'type': 'line', 'name': 'value2'}
-                    ],
-                    'layout': {'title': 'Simple Line Graph',}
-                }
-            ),
+            html.Div(id='dataset-spectrum'),
         ])
     elif tab == 'tab-2':
         return html.Div([
@@ -63,69 +54,102 @@ def render_content(tab):
         ])
 
 
-@app.callback(Output('dataset-entry', 'children'),
-              Input('dataset-selection', 'value'))
-def render_dataset_entry(value):
+@app.callback(Output('label-entry', 'children'),
+              Input('select-dataset', 'value'))
+def render_molecule_entry(value):
     if value == 'NEW':
         return html.Div([
-            dcc.Input(id='dataset-name', type='text', placeholder=''),
+            dcc.Input(id='new-dataset', type='text', placeholder=''),
             html.Br(),
             html.Label("Molecule"),
-            dcc.Input(id='molecule', type='text', placeholder=''),
+            dcc.Input(id='new-molecule', type='text', placeholder=''),
             html.Br(),
             html.Label("Concentration"),
-            dcc.Input(id='concentration', type='text', placeholder='')
+            dcc.Input(id='new-concentration', type='text', placeholder='')
         ])
         
     elif value != None:
+        global dataset                                                                          # dataset choice is reflected throughout application
         dataset = dbi.select_dataset(value)
 
-        molecules = dataset.index.get_level_values('Molecule').unique().to_numpy()
+        molecules = dataset.index.get_level_values('Molecule').unique().to_numpy()              # list unique molecule labels within dataset
         molecule_options = [{'label': name, 'value': name} for name in molecules]
-        molecule_options.append({'label': 'Create New (enter name below...)', 'value': 'NEW'})
+        molecule_options.append({'label': 'Create New (enter name below...)', 'value': 'NEW'})  # allow choice to assign new molecule label
 
-        concentrations = dataset.index.get_level_values('Concentration').unique().to_numpy()
+        return html.Div([
+            html.Label("Molecule"),
+            dcc.Dropdown(id='select-molecule', options=molecule_options),                       # allow choice among prior names or new name
+            html.Div(id='create-molecule'),                                                     # if 'NEW' render text input box here
+            html.Br(),
+            html.Div(id='select-concentration')                                                 # render appropriate input to select corresponding concentration
+        ])
+
+    else:
+        return html.Div([
+
+        ])
+
+
+@app.callback(Output('dataset-spectrum', 'children'),
+              Input('select-dataset', 'value'))
+def render_dataset_plot(value):
+    if value != None:
+        if value != 'NEW':
+            dataset = dbi.select_dataset(value)
+            return dcc.Graph(
+                id='spectrum',
+                figure=px.line(x=dataset.columns.values, y=list(dataset.values[0:3]), labels=['SP_0', 'SP_1', 'SP_2'])
+            )
+
+
+@app.callback(Output('create-molecule', 'children'),
+              Input('select-molecule', 'value'))
+def new_molecule_entry(value):
+    if value == 'NEW':
+        return html.Div(
+            dcc.Input(id='new-molecule', type='text', placeholder='')
+        )
+    else:
+        return html.Div([
+
+        ])
+
+
+@app.callback(Output('select-concentration', 'children'),
+              Input('select-molecule', 'value'))                                        # FIXME: when changing selected molecule will not update concentration options
+def render_concentration_entry(value):
+    if value == 'NEW':
+        return html.Div([
+            html.Label("Concentration"), 
+            dcc.Input(id='new-concentration', type='text', placeholder='')
+        ])
+    elif value != None:
+        concentrations = dataset.loc[value].index.get_level_values('Concentration').unique().to_numpy()
         concentration_options = [{'label': name, 'value': name} for name in concentrations]
         concentration_options.append({'label': 'Create New (enter name below...)', 'value': 'NEW'})
 
         return html.Div([
-            html.Label("Molecule"),
-            dcc.Dropdown(id='prev-molecule', options=molecule_options),
-            html.Div(id='new-molecule'),
-            html.Br(),
             html.Label("Concentration"), 
-            dcc.Dropdown(id='prev-concentration', options=concentration_options),
-            html.Div(id='new-concentration')
+            dcc.Dropdown(id='select-concentration', options=concentration_options),
+            html.Div(id='create-concentration')
         ])
 
+
+@app.callback(Output('create-concentration', 'children'),
+              Input('select-concentration', 'value'))
+def new_concentration_entry(value):
+    if value == 'NEW':
+        return html.Div([
+            dcc.Input(id='new-concentration', type='text', placeholder='')
+        ])
     else:
         return html.Div([
 
         ])
 
+# must have submit button callback 
+## consider input from new-concentration, select-molecule, new-molecule, select-concentration, new-concentration id's
 
-@app.callback(Output('new-molecule', 'children'),
-              Input('prev-molecule', 'value'))
-def new_molecule_option(value):
-    if value == 'NEW':
-        html_element = html.Div(
-            dcc.Input(id='new_molecule', type='text', placeholder='')
-        )
-    else:
-        html_element = html.Div()
-    return html_element
-
-
-@app.callback(Output('new-concentration', 'children'),
-              Input('prev-concentration', 'value'))
-def new_concentration_option(value):
-    if value == 'NEW':
-        html_element = html.Div(
-            dcc.Input(id='new_concentration', type='text', placeholder='')
-        )
-    else:
-        html_element = html.Div()
-    return html_element
 
 if __name__ == '__main__':
     app.run_server(debug=True)
