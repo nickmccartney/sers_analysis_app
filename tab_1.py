dfdfee95a294fd2a4f90c49dcafe988c1805d38a
nickmccartney <nickmcc3@gmail.com>
2021-03-22 15:29:06 -0400

    Optimize callbacks to define import data

--- tab_1.py
+++ tab_1.py
@@ -1,3 +1,4 @@
+import dash
 import dash_html_components as html
 import dash_core_components as dcc
 import plotly.express as px
@@ -19,108 +20,139 @@ def render_tab():
             options=dataset_options,
             placeholder='Select a Dataset...'
         ),
+        html.Div([
+            dcc.Input(
+                id='new-dataset',
+                type='text',
+                placeholder='Enter a dataset name...',
+            )
+        ],
+            id='new-dataset-container',
+            style={'display': 'none'}
+        ),
+
         html.Br(),
 
-        html.Div(id='label-entry'),
+        # html.Label("Molecule"),
+        html.Div([
+            # html.Label("Molecule"),
+            dcc.Dropdown(
+                id='select-molecule',
+                placeholder='Select a Molecule...'
+            )
+        ],
+            id='select-molecule-container',
+            style={'display': 'block'}
+        ),
+
+        html.Div([
+            dcc.Input(
+                id='new-molecule', 
+                type='text', 
+                placeholder='Enter a molecule...'
+            )
+        ],
+            id='new-molecule-container',
+            style={'display': 'none'}
+        ),
+
+        html.Br(),
+
+        # html.Label("Concentration"),
+        html.Div([
+            dcc.Dropdown(
+                id='select-concentration',
+                placeholder='Select a Concentration...'
+        )
+        ],
+            id='select-concentration-container',
+            style={'display': 'block'}
+        ),
+
+        html.Div(
+            dcc.Input(
+                id='new-concentration', 
+                type='text', 
+                placeholder='Enter a concentration...'
+            ),
+            id='new-concentration-container',
+            style={'display': 'none'}
+        )
+
 
         # html.Div(id='dataset-spectrum'),
     ])
 
-@app.callback(Output('label-entry', 'children'),
+@app.callback(Output('new-dataset-container', 'style'),
               Input('select-dataset', 'value'))
-def render_molecule_entry(value):
+def update_dataset_input(value):
+    enabled_style = {'display': 'block'}
+    disabled_style = {'display': 'none'}
+
     if value == 'NEW':
-        return html.Div([
-            dcc.Input(id='new-dataset', type='text', placeholder=''),
-            html.Br(),
-            html.Label("Molecule"),
-            dcc.Input(id='new-molecule', type='text', placeholder=''),
-            html.Br(),
-            html.Label("Concentration"),
-            dcc.Input(id='new-concentration', type='text', placeholder='')
-        ])
-        
-    elif value != None:
-        global dataset                                                                          # FIXME: should find better solution : https://dash.plotly.com/sharing-data-between-callbacks
-        dataset = dbi.select_dataset(value)
+        return enabled_style
+    else:
+        return disabled_style
+
+
+@app.callback(Output('select-molecule', 'options'),
+              Output('select-molecule-container', 'style'),
+              Output('new-molecule-container','style'),
+              Input('select-dataset', 'value'),
+              Input('select-molecule', 'value'))
+def update_molecule_input(dataset_value, molecule_value):
+    enabled_style = {'display': 'block'}
+    disabled_style = {'display': 'none'}
+    options = []
+
+    if dataset_value == 'NEW':
+        return options, disabled_style, enabled_style
+    elif dataset_value != None:
+        dataset = dbi.select_dataset(dataset_value)
 
         molecules = dataset.index.get_level_values('Molecule').unique().to_numpy()              # list unique molecule labels within dataset
-        molecule_options = [{'label': name, 'value': name} for name in molecules]
-        molecule_options.append({'label': 'Create New (enter name below...)', 'value': 'NEW'})  # allow choice to assign new molecule label
+        options = [{'label': name, 'value': name} for name in molecules]
+        options.append({'label': 'Create New (enter name below...)', 'value': 'NEW'})  # allow choice to assign new molecule label
 
-        return html.Div([
-            html.Label("Molecule"),
-            dcc.Dropdown(id='select-molecule', options=molecule_options),                       # allow choice among prior names or new name
-            html.Div(id='create-molecule'),                                                     # if 'NEW' render text input box here
-            html.Br(),
-            html.Div(id='select-concentration')                                                 # render appropriate input to select corresponding concentration
-        ])
-
+        if molecule_value == 'NEW':
+            return options, enabled_style, enabled_style
+        elif molecule_value != None:
+            return options, enabled_style, disabled_style
+        else:
+            return options, enabled_style, disabled_style
     else:
-        return html.Div([
-
-        ])
-
-
-# @app.callback(Output('dataset-spectrum', 'children'),
-#               Input('select-dataset', 'value'))
-# def render_dataset_plot(value):
-#     if value != None:
-#         if value != 'NEW':
-#             dataset = dbi.select_dataset(value)
-#             return dcc.Graph(
-#                 id='spectrum',
-#                 figure=px.line(x=dataset.columns.values, y=list(dataset.values[0:3]), labels=['SP_0', 'SP_1', 'SP_2'])
-#             )
-
-
-@app.callback(Output('create-molecule', 'children'),
-              Input('select-molecule', 'value'))
-def new_molecule_entry(value):
-    if value == 'NEW':
-        return html.Div(
-            dcc.Input(id='new-molecule', type='text', placeholder='')
-        )
-    else:
-        return html.Div([
-
-        ])
-
-
-@app.callback(Output('select-concentration', 'children'),
-              Input('select-molecule', 'value'))                                        # FIXME: when changing selected molecule will not update concentration options
-def render_concentration_entry(value):
-    if value == 'NEW':
-        return html.Div([
-            html.Label("Concentration"), 
-            dcc.Input(id='new-concentration', type='text', placeholder='')
-        ])
-    elif value != None:
-        print(value)
-        concentrations = dataset.loc[value].index.get_level_values('Concentration').unique().to_numpy()
-        concentration_options = [{'label': name, 'value': name} for name in concentrations]
-        concentration_options.append({'label': 'Create New (enter name below...)', 'value': 'NEW'})
-        print(concentration_options)
-
-        return html.Div([
-            html.Label("Concentration"), 
-            dcc.Dropdown(id='select-concentration', options=concentration_options),
-            html.Div(id='create-concentration')
-        ])
-    else: 
-        return html.Div([
-            
-        ])
-
-
-@app.callback(Output('create-concentration', 'children'),
+        return options, disabled_style, disabled_style
+      
+    
+@app.callback(Output('select-concentration', 'options'),
+              Output('select-concentration-container', 'style'),
+              Output('new-concentration-container', 'style'),
+              Input('select-dataset', 'value'),
+              Input('select-molecule', 'value'),
               Input('select-concentration', 'value'))
-def new_concentration_entry(value):
-    if value == 'NEW':
-        return html.Div([
-            dcc.Input(id='new-concentration', type='text', placeholder='')
-        ])
-    else:
-        return html.Div([
+def update_concentration_input(dataset_value, molecule_value, concentration_value):
+    enabled_style = {'display': 'block'}
+    disabled_style = {'display': 'none'}
+    options = []
 
-        ])
+
+    if dataset_value == 'NEW':
+        return options, disabled_style, enabled_style
+    elif dataset_value != None and molecule_value != None:
+        if molecule_value == 'NEW':
+            return options, disabled_style, enabled_style
+        else:
+            dataset = dbi.select_dataset(dataset_value)
+
+            molecules = dataset.loc[molecule_value].index.get_level_values('Concentration').unique().to_numpy()              # list unique molecule labels within dataset
+            options = [{'label': name, 'value': name} for name in molecules]
+            options.append({'label': 'Create New (enter name below...)', 'value': 'NEW'})  # allow choice to assign new molecule label
+
+            if concentration_value == 'NEW':
+                return options, enabled_style, enabled_style
+            elif concentration_value != None:
+                return options, enabled_style, disabled_style
+            else:
+                return options, enabled_style, disabled_style
+    else:
+        return options, disabled_style, disabled_style
